6b76d14ff66bb9c69226fa7a75d7b29886d90571
luispadron <luispadronedu@gmail.com>
2015-09-04 00:56:22 -0400

    1.0
    Bandcamp downloading GUI application.
    
    Downloads the streaming MP3 files from a Bandcamp url

---
+++ bandloader.py
@@ -0,0 +1,231 @@
+import os
+import re
+import sys
+import json
+import platform
+import subprocess
+import urllib.request
+
+import wgetter
+import mutagen
+from mutagen.mp3 import MP3
+from mutagen.id3 import APIC
+from mutagen.easyid3 import EasyID3
+
+
+# scrape the websites HTML
+def get_html_data(query, source, brackets=False):
+    """
+    Scrapes Bandcamp site for query
+
+    :param query:
+    :param source:
+    :param brackets:
+    :return:
+    """
+    try:
+        if brackets:
+            return json.loads("[{" + (re.findall(query + "[ ]?: \[?\{(.+)\}\]?,", source, re.MULTILINE)[0] + "}]"))
+        return re.findall(query + "[ ]?: ([^,]+)", source, re.DOTALL)[0]
+    except:
+        print("Unable to load site and get source")
+        sys.exit(0)
+
+
+# get the track titles from scraped HTML and save it to a list
+def get_track_titles(tracks):
+    """
+    Gets track titles and appends it to a new
+    list, returns list
+
+    :param tracks:
+    :return:
+    """
+    track_t = []
+    for track in tracks:
+        track_t.append(track['title'])
+    return track_t
+
+
+def fix_release_date(date_list):
+    """
+    Deletes the hour and time from date
+    we got from website, just for encoding
+
+    :param date_list:
+    :return:
+    """
+    date_list = date_list.split(" ")
+    new_date_list = []
+    for i in range(3):
+        new_date_list.append(date_list[i])
+    return new_date_list
+
+
+def create_dir(directory, album_title):
+    """
+    Create the directory for saving downloaded files
+
+
+    :param directory: sent directory that user selected
+    :param album_title: title of the album (folder name)
+    :return: file_path
+    """
+    file_path = directory + "\\" + "Bandloader Downloads" + "\\" + album_title + "\\"
+    print(file_path)
+
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    os.path.expanduser(file_path)
+
+    return file_path
+
+
+def collect_album_info(url):
+    """
+    Collects information by crawiling through the website
+    stores information into album_data dictionary and then
+    returns information
+
+    :param url:
+    :return album_data:
+    """
+    tmp_site = urllib.request.urlopen(url)
+    site_source = tmp_site.read().decode('utf-8')
+    tmp_site.close()
+    album_data = {}
+    album_data['track_info'] = get_html_data('trackinfo', site_source, True)
+    album_data['track_titles'] = get_track_titles(album_data['track_info'])
+    album_data['artist'] = get_html_data("artist", site_source).replace('"', '')
+    album_data['title'] = get_html_data("album_title", site_source).replace('"', '')
+    album_data['release_date'] = get_html_data("release_date", site_source).replace('"', '')
+    album_data['cover_url'] = get_html_data("artFullsizeUrl", site_source).replace('"', '')
+    album_data['release_date'] = fix_release_date(album_data['release_date'])
+    return album_data
+
+
+def download_tracks(track_info, track_titles, directory):
+    """
+    Downloads the track that was sent to it
+    Skips over tracks that have alread been downloaded
+
+    :param track_info:
+    :param track_titles:
+    :param directory:
+    :return:
+    """
+    print("Downloading....")
+    # gets the track download urls from
+    # track_info then downlads
+    download_url = track_info['file']['mp3-128']
+    # downloads the mp3 from url, creates
+    # a temporary file in the directory
+    tmp_file = wgetter.download(download_url, outdir=directory)
+    # create the file name with path and .mp3
+    file_name = directory + "\\" + track_titles + ".mp3"
+    # if file already exists, we skip that file and delete the tmp_file
+    if os.path.isfile(file_name):
+        print("Skipping file: " + file_name + " already exists.")
+        pass
+    else:
+        # replace the name of tmp_file with
+        # track title and .mp3
+        os.rename(tmp_file, file_name)
+        print("\nDone downloading track\n")
+
+
+def download_album_cover(album_cover, directory):
+    """
+    Downloads album cover from URL
+    :param album_cover:
+    :param directory:
+    :return:
+    """
+    print("\nDownloading album cover...\n")
+    tmp_file = wgetter.download(album_cover, outdir=directory)
+    file_name = directory + "\\" + "cover.jpg"
+    # if file already exists, we skip that file and delete the tmp_file
+    if os.path.isfile(file_name):
+        os.remove(tmp_file)
+        print("Skipping file: " + file_name + " already exists.")
+        return file_name
+    else:
+        os.rename(tmp_file, file_name)
+        print("\nDone downloading album cover!\n")
+        return file_name
+
+
+def encode_tracks(album_info, directory):
+    """
+    Using mutagen this encodes the .MP3
+    files so that Itunes and other
+    players can know what Artist,
+    album, etc are.
+
+    :param album_info:
+    :param directory:
+    :return:
+    """
+    print("Encoding...")
+    i = 0
+    for track in album_info['track_titles']:
+        file_name = directory + track + ".mp3"
+        try:
+            file_to_encode = EasyID3(file_name)
+        except mutagen.id3.ID3NoHeaderError:
+            print("\nFile didnt have ID3 tag, tagging now\n")
+            file_to_encode = mutagen.File(file_name, easy=True)
+            file_to_encode.add_tags()
+        # add the mp3 tags to the audio files
+        file_to_encode['tracknumber'] = str((i + 1))
+        file_to_encode['title'] = track
+        file_to_encode['artist'] = album_info['artist']
+        file_to_encode['album'] = album_info['title']
+        file_to_encode['date'] = album_info['release_date'][2]
+        file_to_encode.save()
+        file_to_encode = MP3(file_name)
+        print(album_info['cover'])
+        cover_data = open(album_info['cover'], 'rb').read()
+        file_to_encode.tags.add(
+            APIC(
+                encoding=3,
+                mime='image/jpeg',
+                type=3,
+                desc=u'Cover',
+                data=cover_data
+            )
+        )
+        file_to_encode.save()
+        i += 1
+    print("\nEncoding finished!\n")
+
+
+def clean_up_files(path):
+    """
+    Deletes all temporary files that may have been
+    created
+
+    :param path:
+    :return:
+    """
+    file_list = [f for f in os.listdir(path) if f.endswith(".tmp")]
+
+    for file in file_list:
+        os.remove(path + "\\" + file)
+
+
+# opens up the path to the downloaded files
+# when program has finished downloading
+def open_file_path(path):
+    """
+    opens up the path to the downloaded files
+    when program has finished downloading
+
+    :param path:
+    :return:
+    """
+    if platform.system() == "Windows":
+        os.startfile(path)
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", path])
+
---
+++ main.py
@@ -0,0 +1,199 @@
+import sys
+
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+import bandloader_gui
+import bandloader
+
+
+class MainUiClass(QtGui.QMainWindow, bandloader_gui.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super(MainUiClass, self).__init__(parent)
+        self.usr_dir = ""
+        self.cover_url = 0
+        self.setupUi(self)
+
+        # Connect the buttons to methods
+        self.download_button.clicked.connect(self.begin_download)
+        self.dir_button.clicked.connect(self.get_dir)
+        self.clear_button.clicked.connect(self.clear_field)
+        self.action_clear_all.triggered.connect(self.clear_all)
+        self.action_quit.triggered.connect(self.quit_meth)
+
+    def display_error(self, error):
+        if error == 1:
+            QtGui.QMessageBox.about(self, "Error", "Missing either Directory or\n"
+                                                    "Bandcamp URL")
+
+        elif error == 2:
+            QtGui.QMessageBox.about(self, "Error", "Invalid Bandcamp URL\n"
+                                                    "URL must be in this format:\n"
+                                                    "https://____.bandcamp.com/album/____")
+
+    def clear_all(self):
+        """
+        Triggered when user clicks the
+        File -> Clear All option
+
+        Clears all the fields
+        """
+        self.url_edit.setText("")
+        self.dir_edit.setText("")
+
+    def quit_meth(self):
+        """
+        Triggered when user clicks the
+        File -> Quit option
+
+        Quits the application, prompts the user
+        """
+        choice = QtGui.QMessageBox.question(self,
+                                            'Quit',
+                                            'Are you sure you want to quit?\n',
+                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        if choice == QtGui.QMessageBox.Yes:
+            sys.exit(0)
+        else:
+            pass
+
+    def clear_field(self):
+        """
+        Triggered when clicking clear button
+
+        Clears the URL field
+        """
+        self.url_edit.setText("")
+
+    def update_progress_bar(self, prog):
+
+        self.progress = prog / len(self.album['track_titles']) * 100
+
+        if self.progress == 100:
+            self.progress = 90
+
+        self.progress_bar.setValue(self.progress)
+
+    def get_dir(self):
+        """
+        Triggered when clicking on the browse dir button
+
+        Opens up explorer for user to select
+        the directory they would like to save the
+        files to
+
+        Then sets the dir_labe in the GUI to said directory
+        """
+        self.usr_dir = QtGui.QFileDialog.getExistingDirectory(self,
+                                                              'Select where to '
+                                                              'save files')
+        self.dir_edit.setText(self.usr_dir)
+
+    def begin_download(self):
+        i = 0
+        url = str(self.url_edit.text())
+        file_dir = str(self.dir_edit.text())
+
+        if not url or not file_dir:
+            self.display_error(error=1)
+        elif "/album/" not in url:
+            self.display_error(error=2)
+
+        self.album = bandloader.collect_album_info(url)
+        self.final_dir = bandloader.create_dir(file_dir, self.album['title'])
+        self.progress_label.setText("Downloading songs... This takes a bit.")
+        self.d_thread = DownloadThread(album_data=self.album, directory=self.final_dir)
+        self.connect(self.d_thread, QtCore.SIGNAL('PROGRESS'), self.update_progress_bar)
+        self.connect(self.d_thread, QtCore.SIGNAL('THREAD DONE'), self.finish_up)
+        # Start download thread
+        self.d_thread.start()
+
+        # Check to see if we have a url to download the album cover with
+        if not self.album['cover_url']:
+            QtGui.QMessageBox.about(self, "Dang!", "No album cover found\n"
+                                                   "Continuing...")
+            self.cover_url = -1
+
+    def finish_up(self):
+        """
+        Do finishing tasks, such as download album cover,
+        encode tracks, stop threads, and clean up directory
+
+        """
+
+        # Stop the thread
+        self.d_thread.quit()
+
+        # Skip downloading cover URL if we don't have it
+        if self.cover_url == -1:
+            print("Cant download cover")
+        else:
+            self.progress_label.setText("Downloading album cover.")
+            self.album['cover'] = self.download_cover(self.album['cover_url'], self.final_dir)
+
+        # Encode the mp3 files
+        self.progress_label.setText("Encoding files...")
+        self.encode(self.album, self.final_dir)
+
+        # Clean up files
+        bandloader.clean_up_files(self.final_dir)
+
+        # Finish the progress bar
+        self.progress_bar.setValue(100)
+        QtGui.QMessageBox.about(self, "Done", "Done downloading & encoding!\n"
+                                              "Remember, please support the Artists")
+        self.progress_label.setText("Done!")
+        bandloader.open_file_path(self.final_dir)
+
+    @staticmethod
+    def download_cover(cover_url, directory):
+        """
+        Downloads the album cover after downloading tracks
+        """
+        result = bandloader.download_album_cover(cover_url, directory)
+
+        return result
+
+    @staticmethod
+    def encode(album, directory):
+        """
+        Encodes the tracks using mutagen
+        """
+        bandloader.encode_tracks(album, directory)
+
+
+class DownloadThread(QtCore.QThread):
+
+    """
+    Threading for the download function inside of
+    bandloader.py
+
+    Without threading GUI locks up :(
+
+    We grab
+    """
+
+    def __init__(self, parent=None, album_data=None, directory=None):
+        super(DownloadThread, self).__init__(parent)
+        self.album_info = album_data
+        self.directory = directory
+
+    def run(self):
+        i = 0
+        perc = 0
+        for track in self.album_info['track_info']:
+            perc += 1
+            self.emit(QtCore.SIGNAL('PROGRESS'), perc)
+            bandloader.download_tracks(track, self.album_info['track_titles'][i],
+                                       self.directory)
+            i += 1
+
+        self.emit(QtCore.SIGNAL('THREAD DONE'))
+
+
+if __name__ == '__main__':
+    a = QtGui.QApplication(sys.argv)
+    app = MainUiClass()
+    app.show()
+    a.exec()
